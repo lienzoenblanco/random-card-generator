2c19c03d599c9bfd929a8582f8e4e99b102e3e87
lienzoenblanco <raquelgarciatrives@gmail.com>
2022-01-10 17:57:30 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,43 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  generateSuit();
+  generateValue();
+};
+
+let generateSuit = () => {
+  let suit = [
+    { icon: "♦", color: "red" },
+    { icon: "♥", color: "red" },
+    { icon: "♠", color: "black" },
+    { icon: "♣", color: "black" }
+  ];
+  let suitIndex = Math.floor(Math.random() * suit.length);
+  let suitList = document.querySelectorAll(".suit");
+
+  suitList.forEach(suitNode => {
+    suitNode.innerHTML = suit[suitIndex].icon;
+    suitNode.classList.add(`c-${suit[suitIndex].color}`);
+  });
+};
+
+let generateValue = () => {
+  let value = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "K",
+    "Q"
+  ];
+  let valueIndex = Math.floor(Math.random() * value.length);
+  document.querySelector(".value").innerHTML = value[valueIndex];
 };
